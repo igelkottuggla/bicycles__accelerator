8fb4e8684a3cb0c5c361c3961ca11f515cfa9c6b
NataliaSova <rot.fuchslein@gmail.com>
2021-10-22 09:56:21 +0500

    навигация + главный блок. мобильное меню, js для мобильного меню, + js для скролла

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,41 @@
-'use strict';
+"use strict";
+
+const header = document.querySelector(".header");
+const openBtn = document.querySelector(".header__button");
+const menuEl = document.querySelector(".header__nav");
+const headerNavLinks = document.querySelectorAll(".nav__link");
+
+const gap = 100;
+
+
+if (header) {
+  header.classList.remove("header--no-js");
+
+  openBtn.addEventListener("click", () => {
+    header.classList.toggle("header--open");
+  });
+
+  headerNavLinks.forEach((link) => {
+    link.addEventListener("click", () =>
+      header.classList.remove("header--open")
+    );
+  });
+}
+
+if (headerNavLinks) {
+  headerNavLinks.forEach((link) => {
+    link.addEventListener("click", (scrollEvent) => {
+      scrollEvent.preventDefault();
+      const id = scrollEvent.currentTarget.getAttribute("href").slice(1);
+      const element = document.getElementById(id);
+      let position = element.offsetTop - gap;
+
+      window.scrollTo({
+        left: 0,
+        top: position,
+        behavior: "smooth",
+      });
+    });
+  });
+}
+
